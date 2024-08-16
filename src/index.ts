c9a7cf41950f52127faffe44a572d3e20bd509aa
codycrossley <codycrossley@users.noreply.github.com>
2024-08-16 14:12:15 -0400

    Add `v1.1` as a valid `version` choice

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ main().catch((error) => {
 async function main() {
   program
     .argument("<filepath>", "filepath to validate")
-    .addArgument(new Argument("<version>").choices(["v2.0", "v2.0.0"]))
+    .addArgument(new Argument("<version>").choices(["v1.1", "v2.0", "v2.0.0"]))
     .addOption(
       new Option("-f, --format <string>", "file format of file").choices([
         "csv",
