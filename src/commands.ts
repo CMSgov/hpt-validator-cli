19fa7e0eeb5a184cefbcb86ff991084cf92c4124
Mint Thompson <mathompson@mitre.org>
2024-11-05 15:28:48 -0500

    Run lint/prettier on push to any branch, not just main (#25)

--- src/commands.ts
+++ src/commands.ts
@@ -19,7 +19,7 @@ export async function validate(
   const format = getFileFormat(filepath, options)
   if (!format) {
     console.log(
-      `This is not a valid file type. Files must be in a required CMS template format (.json or .csv)`
+      "This is not a valid file type. Files must be in a required CMS template format (.json or .csv)"
     )
     return
   }
